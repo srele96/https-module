d71dd1034d2edef5d3334c3e1e4e7234c82b4a0c
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-05-13 19:13:46 +0200

    add jsdoc comment for getPageState usage

--- client/App.js
+++ client/App.js
@@ -14,7 +14,20 @@ const fetchReactPackages = () =>
 
 function App() {}
 
-// component name must be unique and key must be unique per component
+/**
+ * Should return an object with the following shape:
+ *
+ * @example
+ * {
+ *   [componentName]: {
+ *     [key]: Promise { <pending> }
+ *   }
+ * }
+ *
+ * A component name must be unique and key must be unique per component.
+ * The component App should use the state from App object.
+ * The component Test should NOT use the state from App object.
+ */
 App.getPageState = function getPageState() {
   return {
     App: {

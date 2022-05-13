8c3703dc04dccebd5d0f5507a167cfa30b95784b
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-05-13 10:43:55 +0200

    Add server side page state handling
    I created a function getPageState that should be on page component.
    On the server when the page is request, the page component calls the
    getPageState function and resolves promises that should request data.
    The page is rendered with initial data structured like object returned
    from getPageState.
    
    getInitialState resolves and maps the resolved data to the provided
    strcture.
    
    getInitialHTML exposes initial state on global level and renders the
    component on the server. Initial state is available on the server and
    the client.

--- client/App.js
+++ client/App.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-const { useState, useEffect } = require('react');
 const { createElement: e } = require('react');
 
 const fetchReactPackages = () =>
@@ -13,72 +12,25 @@ const fetchReactPackages = () =>
       .catch((error) => reject(error.message));
   });
 
-const isServer = () => typeof window === 'undefined';
+function App() {}
 
-function getInitialState(key, defaultValue) {
-  if (isServer()) {
-    // server-side data is always fresh because it is fetched with every request
-    return globalThis.__INITIAL_STATE__[key];
-  } else {
-    // use fresh state after ssr
-    // if the component remounts after the ssr, the state is not fresh anymore
-    // loading is false after server fetches the data, but
-    // if the component remounts the loading should be true
-    // window['loading'] will be false on every subsequent remount and client
-    // will never re-fetch the data
-    if (typeof window.__INITIAL_STATE__[key] !== 'undefined') {
-      const state = window.__INITIAL_STATE__[key];
-      // purge the initial state after using it to avoid stale initial data
-      window.__INITIAL_STATE__[key] = undefined;
-      return state;
-    } else {
-      return defaultValue;
-    }
-  }
-}
-
-function App() {
-  const [loading, setLoading] = useState(getInitialState('loading'));
-  const [data, setData] = useState(getInitialState('data'));
-  const [error, setError] = useState(getInitialState('error'));
-
-  useEffect(() => {
-    if (loading) {
-      fetchReactPackages()
-        .then((data) => {
-          setData(data);
-          setLoading(false);
-        })
-        .catch((error) => {
-          setError(error);
-        });
-    }
-  }, [loading, setData, setLoading, setError]);
-
-  if (loading) {
-    return e('div', {}, 'Loading...');
-  }
-
-  if (error) {
-    return e('div', {}, error);
-  }
-
-  return e(
-    'article',
-    {},
-    e('h1', {}, 'React packages'),
-    data.map((d) => {
-      const { name, version, description } = d.package;
-      return e(
-        'section',
-        { key: name + version },
-        e('h1', {}, name),
-        e('p', {}, description),
-        e('p', {}, `Version: ${version}`)
-      );
-    })
-  );
-}
+// component name must be unique and key must be unique per component
+App.getPageState = function getPageState() {
+  return {
+    App: {
+      data: fetchReactPackages(),
+    },
+    Test: {
+      test: new Promise((resolve) => {
+        setTimeout(() => resolve('Hello'), 1000);
+      }),
+    },
+    Rejected: {
+      rejected: new Promise((resolve, reject) => {
+        setTimeout(() => reject('Hello'), 1000);
+      }),
+    },
+  };
+};
 
 module.exports = App;
-module.exports.fetchReactPackages = fetchReactPackages;
